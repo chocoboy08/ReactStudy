5523441df35b78523ee9aa5ea302867fc0c6b1d1
chocoboy08 <chocoboy0508@gmail.com>
2022-03-29 23:57:12 +0900

    리액트 5장

---
+++ src/ValidationSample.js
@@ -0,0 +1,43 @@
+import React, { Component } from 'react';
+import './ValidationSample.css';
+export default class ValidationSample extends Component {
+    state = {
+        password : '',
+        clicked : 'false',
+        validated : 'false'
+    }
+
+    handleChange = (e) =>{
+        this.setState({
+            [e.target.name]: e.target.value
+        })
+    }
+    
+    handleClick = () =>{
+        this.setState({
+            clicked: 'true',
+            validated: this.state.password === '0000'
+        })
+    }
+
+    handleKeyPress = (e) =>{
+        if(e.key === 'Enter'){
+            this.handleClick();
+        }
+    }
+
+  render() {
+    return (
+      <div>
+          <input
+          type="password"
+          value={this.state.password}
+          name="password"
+          onChange={this.handleChange}
+          onKeyPress={this.handleKeyPress}
+          className={this.state.clicked ? (this.state.validated ? 'success' : 'failure') : ''}/>
+          <button onClick={this.handleClick}>검증하기</button>
+      </div>
+    )
+  }
+}

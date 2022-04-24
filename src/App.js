0601b0b0678ac26d8ad8588c1890c1d45213126a
chocoboy08 <chocoboy0508@gmail.com>
2022-04-24 22:02:41 +0900

    8장

--- src/App.js
+++ src/App.js
@@ -1,30 +1,11 @@
-import React, { Component } from "react";
-import ErrorBoundary from "./ErrorBoundary.js";
-import LifeCycleSample from "./LifeCycleSample.js";
+import React, {useState} from "react";
+import Average from "./Average.js";
 
-function getRandomColor(){
-  return '#' + Math.floor(Math.random() * 16777215).toString(16);
-}
 
-class App extends Component {
-  state = {
-    color: '#000000'
-  }
-  handleClick = () =>{
-    this.setState({
-      color: getRandomColor()
-    });
-  }
-  render(){
-    return(
-      <>
-        <button onClick={this.handleClick}>랜덤 색상</button>
-        <ErrorBoundary>
-          <LifeCycleSample color={this.state.color}/>
-        </ErrorBoundary>
-      </>
-    );
-  }
+const App = () => {
+  return(
+    <Average/>
+  )
 }
 
 export default App;
